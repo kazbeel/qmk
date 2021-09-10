f396c1b0a79e55eca55dcf0a9b79584812b769f3
dvalladolid <diego@zennio.com>
2021-09-10 07:59:50 +0200

    [main] Disable retro tapping

--- crkbd/config.h
+++ crkbd/config.h
@@ -10,10 +10,10 @@
 
 /**
  * Use this to allow LOWER and RAISE keys to perform single-tap even after the
- * TAPPING_TERM when no secondary key es pressed. This is because the TAPPING_TERM for
- * these keys is very slow.
+ * TAPPING_TERM when no secondary key is pressed. This is because the TAPPING_TERM for
+ * these keys is very low.
  */
-#define RETRO_TAPPING
+// #define RETRO_TAPPING
 
 #ifdef RGB_MATRIX_ENABLE
 #  define RGB_DISABLE_WHEN_USB_SUSPENDED false // turn off effects when suspended
