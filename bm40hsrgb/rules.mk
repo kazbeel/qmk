adb6cce12701edff8a0eec87d0a4c1fe6737f617
kazbeel <diego.valladolid@gmail.com>
2022-02-01 23:02:12 +0100

    Remove old OS dependent compilation

--- bm40hsrgb/rules.mk
+++ bm40hsrgb/rules.mk
@@ -6,12 +6,3 @@ EXTRAKEY_ENABLE = yes
 
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
-
-ifeq ($(strip $(OS_WINDOWS)), yes)
-	OPT_DEFS += -DOS_WINDOWS
-endif
-
-ifeq ($(strip $(OS_MAC)), yes)
-	OPT_DEFS += -DOS_MAC
-endif
-
